14fcdb99859b3469155b67ad8e8a7a5fad1e3a0c
nboos <nboos@DESKTOP-4M2RMIC>
2024-05-26 21:28:36 -0400

    SGD completed, learned a lot about how parameters are modified using the derivative of a function. Also learned about how steps work and how the rate of learning affects the model. Good math refresher on the quadratic formula and how it relates to "training" a model using a,b,c as modifiable parameters to decrease the loss.

--- SGD.py
+++ SGD.py
@@ -9,9 +9,43 @@ from PIL import Image
 def pr_eight(x,w):
     return (x*w).sum()
 
-def f(x):
-    return x**2
+def f(t, params):
+    a,b,c = params
+    return a*(t**2)+(b*t)+c
 
-plot_function(f, 'x', 'x**2')
-plt.scatter(-1.5, f(-1.5), color='red')
+def mse(preds, targets):
+    return ((preds-targets)**2).mean()
+
+time = torch.arange(0,20).float();
+speed = torch.randn(20)*3+0.75*(time-9.5)**2+1
+
+def show_preds(preds, ax=None):
+    if ax is None: ax=plt.subplots()[1]
+    ax.scatter(time, speed)
+    ax.scatter(time, to_np(preds), color='red')
+    ax.set_ylim(-300,100)
+
+params = torch.randn(3).requires_grad_()
+orig_params = params.clone()
+
+lr = 2e-5
+
+def apply_step(params, prn=True):
+    preds = f(time, params)
+    loss = mse(preds, speed)
+    loss.backward()
+    params.data -= lr * params.grad.data
+    params.grad = None
+    if prn: print(loss.item())
+    return preds
+
+for i in range(10):
+    apply_step(params)
+
+params = orig_params.detach().requires_grad_()
+_,axs = plt.subplots(1,4,figsize=(12,3))
+
+for ax in axs:
+    show_preds(apply_step(params, False), ax)
+plt.tight_layout()
 plt.show()
